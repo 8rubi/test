c421a8394127bbe45444ef89adfa7c6d68384713
8rubi <147278991+8rubi@users.noreply.github.com>
2024-09-03 16:12:19 +0200

    test.php

---
+++ test.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title> Hello world</title>
+</head>
+<body>
+
+<form action="welcome.php" method="POST">
+    Name: <input type="text" name="name"><br>
+    firstinteger: <input type="int" name="firstinteger"><br>
+    secondinteger: <input type="int" name="secondinteger"><br>
+    <input type="submit">
+</form>
+
+
+</body>
+</html>
